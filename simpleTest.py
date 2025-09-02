4de6e5fd1b2d34d7650b06b5349ba9be0e96c344
Mang0kim <89622930+Mang0kim@users.noreply.github.com>
2025-09-03 00:08:17 +0900

    Update simpleTest.py

--- simpleTest.py
+++ simpleTest.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-import time
+import time, os, sys
 import RPi.GPIO as GPIO
 from hx711 import HX711
 
-# --- 핀 번호(BCM) ---
 DOUT_PIN = 5   # HX711 DT
 SCK_PIN  = 6   # HX711 SCK
 
@@ -12,11 +11,24 @@ GPIO.setmode(GPIO.BCM)
 
 hx = HX711(dout_pin=DOUT_PIN, pd_sck_pin=SCK_PIN)
 
+def restart_program():
+    """재시작 함수"""
+    GPIO.cleanup()
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
+
 try:
     while True:
-        val = hx.get_raw_data_mean(readings=10)  # 평균 10회
-        # 라이브러리는 실패 시 False를 반환할 수 있음
-        print(val if val is not False else "invalid")
+        try:
+            val = hx.get_raw_data_mean(readings=4)
+            if val is not False:
+                print(val)
+            else:
+                print("invalid")
+        except Exception as e:
+            print("Error:", e, " → 프로그램 재시작")
+            restart_program()
         time.sleep(0.1)
+
 finally:
     GPIO.cleanup()
